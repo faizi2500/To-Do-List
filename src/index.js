83acc7944e033f397b4cbe2a594eb3430e12b97a
Faizan <faizan.zahid2500@gmail.com>
2021-08-18 15:04:13 +0500

    Add html, css and javascript inside the src folder

--- src/index.js
+++ src/index.js
@@ -1,14 +1,70 @@
-import _ from 'lodash';
 import './style.css';
 
+const body = document.getElementsByTagName('body');
+const form = document.getElementById('my-form');
+const taskName = document.getElementById('my-form');
+const allTasks = document.getElementById('all-tasks');
+const refresh = document.querySelector('fa-sync-alt');
+const insert = document.getElementById('enter-task');
+const addTask = document.getElementById('task');
+const clear = document.getElementById('all-completed');
 
-function component() {
-  const element = document.createElement('div');
+const taskList = [
+  {
+    description: 'Wash clothes',
+    completed: false,
+    id: 0,
+  },
+  {
+    description: 'Spend Time With Family',
+    completed: false,
+    id: 1,
+  },
+  {
+    description: 'Read Books',
+    completed: false,
+    id: 2,
+  },
+];
 
-  // Lodash now imported by this script
-  element.innerHTML = _.join(['Hello', 'I am lodash library'], ' ');
-  element.classList.add('hello');
-  return element;
-}
+const displayTasks = () => {
+  allTasks.innerHTML = '';
+  for (let i = 0; i < taskList.length; i++) {
+    const each = taskList[i];
+    const list = `<div class="eachTask">
+      <div class="group-list">
+      <input type="checkbox" class="box" id="list-box" name="list-box">
+        <p class="task-name">${each.description}</p>
+      </div> 
+      <button class="menu-icon" id="${each.id}"><i class="fas fa-ellipsis-v"></i></button>
+    </div>
+    <hr>`;
 
-document.body.appendChild(component());
+
+    allTasks.innerHTML += list;
+  }
+};
+
+// Add tasks to the taskList array.
+const addTaskList = () => {
+  const description = addTask.value;
+  const completed = false;
+  if (description === '') {
+    addTask.setAttribute('required', '');
+    addTask.style.border = 'thin solid red';
+  } else {
+    const id = taskList.length;
+    const task = { description, completed, id };
+    taskList.push(task);
+    addTask.style.border = 'thin solid black';
+    displayTasks();
+  }
+  form.reset();
+};
+
+insert.addEventListener('click', (e) => {
+  e.preventDefault();
+  addTaskList();
+});
+
+window.onload = displayTasks();
